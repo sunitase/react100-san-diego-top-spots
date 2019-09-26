a56b17aa6f7f1210ec0c1f07d58adad7bf1fa35c
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-25 17:20:38 -0700

    works need to do bootstrap/css

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'></div>
-    );
-  }
-}
+  constructor(props){
+    super(props);
+    this.state = {
+      topspots: []
+     };
+    }
 
+     componentWillMount() {
+     axios
+     .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+     .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
+    };
+
+  
+      render(){
+        return ( 
+          <div className='App'>
+            <jumbotron>
+              <div>
+              <h1>San Diego Top Spots</h1>
+              <h4>A list of top 30 places to see in San Diego, California</h4>
+             <div>
+              <pre> { 
+              this.state.topspots.map(topspot => (
+              <TopSpot
+              key={topspot.id}
+              name={topspot.name}
+              description={topspot.description}
+              location={topspot.location} />
+              )) }</pre>
+              </div>
+          </div>
+          </jumbotron>
+          </div>
+        );
+      }
+}
+  
 export default App;
---
+++ src/topspot.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => (
+<div className='well'>
+    <h4>{props.name}</h4>
+    <p>{props.description}</p>
+    <button>
+        <a href = {'https://maps.google.com/?q' + props.location}> Location </a>
+    </button>
+</div>
+);
